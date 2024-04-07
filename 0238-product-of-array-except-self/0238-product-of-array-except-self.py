d167d1a8d4a2199ab1a11e5db9c3033ba60200ec
Aishwarya Kanaparthi <143132803+AishKanaparthi@users.noreply.github.com>
2024-04-06 21:46:01 -0500

    Time: 305 ms (5.09%), Space: 34.8 MB (5.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,67 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res= [1]* len(nums)
-        res2=[1]* len(nums)
-        
+        res1= [[1] for i in range(len(nums))]
+        res2 = [[1] for i in range(len(nums))]
+        final_res = []
         c=1
         d=1
+        
         for i in range(len(nums)):
-            res[i] = c
-            c*=nums[i]
-        for i in range(len(nums)-1, -1, -1):
+            res1[i] =c
+            c *=nums[i]
+        for i in range(len(nums)-1,-1,-1):
             res2[i] = d
-            d*= nums[i]
-        for i in range(len(res)):
-            res[i]*= res2[i]
-        return res
+            d *= nums[i]
+        for i in range(len(res1)):
+            final_res.append(res1[i]* res2[i])
+        
+            
+        
+        
+            
+        return final_res
+               
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res= [1]* len(nums)
+#         res2=[1]* len(nums)
+        
+#         c=1
+#         d=1
+#         for i in range(len(nums)):
+#             res[i] = c
+#             c*=nums[i]
+#         for i in range(len(nums)-1, -1, -1):
+#             res2[i] = d
+#             d*= nums[i]
+#         for i in range(len(res)):
+#             res[i]*= res2[i]
+#         return res
         
         
